b125944ac13b100e090e3e5a9a819cbc9b447af5
Stephan Heijl <sheijl@bio-prodict.nl>
2018-10-03 00:32:55 +0200

    Added newick files to test file for interoperability

--- src/test_tree_parsing.rs
+++ src/test_tree_parsing.rs
@@ -1,12 +1,10 @@
 #[cfg(test)]
 mod tests {
     use::tree;
-    use::utils;
 
     #[test]
     fn test_construct_tree_from_file_1() {
-        let filename = String::from("/home/stephan/newick_trees/1.tree");
-        let tree_file = utils::load_tree_file(filename);
+        let tree_file = String::from("((raccoon:19.19959,bear:6.80041):0.84600,((sea_lion:11.99700, seal:12.00300):7.52973,((monkey:100.85930,cat:47.14069):20.59201, weasel:18.87953):2.09460):3.87382,dog:25.46154);");
         let parsed_tree = tree::Tree::parse(tree_file);
 
         let expected_children = vec!(
@@ -28,8 +26,7 @@ mod tests {
     #[test]
     fn test_construct_tree_from_file_2() {
         // Tree 2
-        let filename = String::from("/home/stephan/newick_trees/2.tree");
-        let tree_file = utils::load_tree_file(filename);
+        let tree_file = String::from("(Bovine:0.69395,(Gibbon:0.36079,(Orang:0.33636,(Gorilla:0.17147,(Chimp:0.19268, Human:0.11927):0.08386):0.06124):0.15057):0.54939,Mouse:1.21460):0.10;");
         let parsed_tree = tree::Tree::parse(tree_file);
 
         let expected_children = vec!(
@@ -49,28 +46,24 @@ mod tests {
 
     #[test]
     fn test_construct_tree_from_file_3() {
-        let filename = String::from("/home/stephan/newick_trees/4.tree");
-        let tree_file = utils::load_tree_file(filename);
+        let tree_file = String::from("(Alpha,Beta,Gamma,Delta,,Epsilon,,,);");
         let parsed_tree = tree::Tree::parse(tree_file);
 
-        let expected_children = vec!(
-            vec!(
-                String::from("Alpha"),
-                String::from("Beta"),
-                String::from("Gamma"),
-                String::from("Delta"),
-                String::from(""),
-                String::from("Epsilon"),
-                String::from(""),
-                String::from(""),
-                String::from("")
-            )
+        let expected_leaves = vec!(
+            String::from("Alpha"),
+            String::from("Beta"),
+            String::from("Gamma"),
+            String::from("Delta"),
+            String::from(""),
+            String::from("Epsilon"),
+            String::from(""),
+            String::from(""),
+            String::from("")
         );
         let children = parsed_tree.traverse_children();
 
-        // Depth first check
-        for (c, child) in children.iter().enumerate() {
-            assert_eq!(child[0].0.leaves, expected_children[c]);
+        for (i, leaf) in children[0][0].0.leaves.iter().enumerate() {
+            assert_eq!(leaf, &expected_leaves[i]);
         }
     }
 }
