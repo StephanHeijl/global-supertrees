dd63dc6239987ba6f1d3b7c7ea3192acb7b036ba
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:41:54 +0300

    rustyrobot formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,9 @@
-use std::fs::File;
-use std::io::prelude::*;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
 
-
-pub fn load_tree_file(filename : String) -> String {
+pub fn load_tree_file(filename: String) -> String {
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
@@ -15,12 +14,16 @@ pub fn load_tree_file(filename : String) -> String {
     String::from(contents.trim())
 }
 
-pub fn keys_vec<T : std::cmp::Eq + std::hash::Hash + std::clone::Clone, V>(hm : &HashMap<T, V>) -> Vec<T> {
+pub fn keys_vec<T: std::cmp::Eq + std::hash::Hash + std::clone::Clone, V>(
+    hm: &HashMap<T, V>,
+) -> Vec<T> {
     return hm.keys().cloned().collect();
 }
 
-pub fn vec_to_set<T : std::cmp::Eq + std::hash::Hash + std::clone::Clone>(vector : &Vec<T>) -> HashSet<T> {
-    let mut set : HashSet<T> = HashSet::new();
+pub fn vec_to_set<T: std::cmp::Eq + std::hash::Hash + std::clone::Clone>(
+    vector: &Vec<T>,
+) -> HashSet<T> {
+    let mut set: HashSet<T> = HashSet::new();
     for element in vector.iter() {
         set.insert(element.clone());
     }
